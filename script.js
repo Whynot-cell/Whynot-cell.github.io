93e9ddb78c912c9f875610eb496795f0d32d0747
liaohaiyang1534 <liaohaiyang1534@163.com>
2024-09-30 12:44:27 +0800

    template
    use others template

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-function changeText() {
-    document.getElementById("welcome").innerHTML = "Hello from external JavaScript!";
-    alert("You have successfully linked an external JS file!");
-}
