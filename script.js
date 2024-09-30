346fa41b1abd803327e3970c9c2c261ed1189912
liaohaiyang1534 <104192749+liaohaiyang1534@users.noreply.github.com>
2024-09-30 11:44:31 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+function changeText() {
+    document.getElementById("welcome").innerHTML = "Hello from external JavaScript!";
+    alert("You have successfully linked an external JS file!");
+}
